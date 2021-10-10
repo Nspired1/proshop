57fec52cc959632757db7820b896a2cfb9159da5
Don Spire <don.spire1@gmail.com>
2021-10-09 22:25:22 -0700

    add redux Provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import App from "./App";
+import "./index.css";
+
 import "./bootstrap.min.css";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
-
-
