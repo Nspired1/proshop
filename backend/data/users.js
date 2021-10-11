7673f98d0336f52dbd8b1433ff0787bcc6566a46
Don Spire <don.spire1@gmail.com>
2021-10-10 21:32:49 -0700

    data for user profiles

---
+++ backend/data/users.js
@@ -0,0 +1,27 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Paige Turner",
+    email: "paige@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Taylor Hill",
+    email: "taylor@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Jane Doe",
+    email: "jane@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+
+export default users;
