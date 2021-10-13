f204ff150cf8fcc1f007d7585784cdc2ed0b2cb7
Don Spire <don.spire1@gmail.com>
2021-10-13 13:22:06 -0700

    bug fix for Profile Screen

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -9,10 +9,12 @@ import {
   USER_DETAILS_FAIL,
   USER_DETAILS_REQUEST,
   USER_DETAILS_SUCCESS,
+  USER_DETAILS_RESET,
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
 } from "../constants/userConstants";
+import { ORDER_LIST_MY_RESET } from "../constants/orderConstants";
 import axios from "axios";
 
 export const login = (email, password) => async (dispatch) => {
@@ -50,9 +52,12 @@ export const login = (email, password) => async (dispatch) => {
   }
 };
 
+// logs user out AND resets user details and order list, so it won't show on user Profile for another user logging in.
 export const logout = () => async (dispatch) => {
   localStorage.removeItem("userInfo");
   dispatch({ type: USER_LOGOUT });
+  dispatch({ type: USER_DETAILS_RESET });
+  dispatch({ type: ORDER_LIST_MY_RESET });
 };
 
 export const register = (name, email, password) => async (dispatch) => {
