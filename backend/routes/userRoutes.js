8281e66c7cc4da48cfbe3795dfd0bfe3e57b9270
Don Spire <don.spire1@gmail.com>
2021-10-12 22:08:17 -0700

    add routes get and update profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,12 +4,16 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 // prefix is /api/users
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
