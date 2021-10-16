fc884f0787fe43452bfb9150b16056c21aac1ed8
Don Spire <don.spire1@gmail.com>
2021-10-15 20:36:26 -0700

    fixed bug in render of Users table

--- client/src/screens/UserListScreen.js
+++ client/src/screens/UserListScreen.js
@@ -19,11 +19,10 @@ const UserListScreen = ({ history }) => {
   const { success: successDelete } = userDelete;
 
   useEffect(() => {
+    dispatch(listUsers());
+
     if (userInfo && userInfo.isAdmin) {
       dispatch(listUsers());
-
-      //   console.log("This is listUsers in useEffect of UserListScreen");
-      //   console.log(users);
     } else {
       history.push("/login");
     }
@@ -31,7 +30,6 @@ const UserListScreen = ({ history }) => {
 
   const deleteHandler = (id) => {
     if (window.confirm("Are you sure?")) {
-      console.log("DELETE HANDLER CLICKED");
       dispatch(deleteUser(id));
     }
   };
@@ -54,40 +52,46 @@ const UserListScreen = ({ history }) => {
               <th></th>
             </tr>
           </thead>
+
           <tbody>
-            {users.map((user) => (
-              <tr key={user._id}>
-                <td>{user._id}</td>
-                <td>{user.name}</td>
-                <td>
-                  <a href={`mailto:${user.email}`}>{user.email}</a>{" "}
-                </td>
-                <td>
-                  {" "}
-                  {user.isAdmin ? (
-                    <i className="fas fa-check" style={{ color: "green" }}></i>
-                  ) : (
-                    <i className="fas fa-times" style={{ color: "red" }}>
-                      {" "}
-                    </i>
-                  )}
-                </td>
-                <td>
-                  <LinkContainer to={`/user/${user._id}/edit`}>
-                    <Button variant="light" className="btn-sm">
-                      <i className="fas fa-edit"></i>
+            {/* NOTE: need users && users.map because sometimes on reload users gets nulled */}
+            {users &&
+              users.map((user) => (
+                <tr key={user._id}>
+                  <td>{user._id}</td>
+                  <td>{user.name}</td>
+                  <td>
+                    <a href={`mailto:${user.email}`}>{user.email}</a>{" "}
+                  </td>
+                  <td>
+                    {" "}
+                    {user.isAdmin ? (
+                      <i
+                        className="fas fa-check"
+                        style={{ color: "green" }}
+                      ></i>
+                    ) : (
+                      <i className="fas fa-times" style={{ color: "red" }}>
+                        {" "}
+                      </i>
+                    )}
+                  </td>
+                  <td>
+                    <LinkContainer to={`/admin/user/${user._id}/edit`}>
+                      <Button variant="light" className="btn-sm">
+                        <i className="fas fa-edit"></i>
+                      </Button>
+                    </LinkContainer>
+                    <Button
+                      variant="danger"
+                      className="btn-sm"
+                      onClick={() => deleteHandler(user._id)}
+                    >
+                      <i className="fas fa-trash"></i>
                     </Button>
-                  </LinkContainer>
-                  <Button
-                    variant="danger"
-                    className="btn-sm"
-                    onClick={() => deleteHandler(user._id)}
-                  >
-                    <i className="fas fa-trash"></i>
-                  </Button>
-                </td>
-              </tr>
-            ))}
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </Table>
       )}
