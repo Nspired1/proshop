b29104243bcfbf668e30ab72bca866ff0eeeefa1
Don Spire <don.spire1@gmail.com>
2021-10-20 12:58:33 -0700

    add routes and controller for orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -79,6 +79,24 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc update order to delivered
+// @route POST /api/orders/:id/deliver
+// @access Private/Admin
+const updateOrderToDeliver = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found.");
+  }
+});
+
 // @desc    get logged in user orders
 // @route   GET /api/orders/myorders
 // @access  Private
@@ -87,4 +105,19 @@ const getMyOrders = asyncHandler(async (req, res) => {
   res.json(orders);
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
+// @desc    Get all orders
+// @route   GET /api/orders
+// @access  Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate("user", "id name");
+  res.json(orders);
+});
+
+export {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDeliver,
+  getMyOrders,
+  getOrders,
+};
