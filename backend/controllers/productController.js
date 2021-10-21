598d9f306be27b8fd12f96baaa2323c9edde311c
Don Spire <don.spire1@gmail.com>
2021-10-21 14:50:33 -0700

    add product Pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,6 +5,9 @@ import Product from "../models/ProductModel.js";
 // @route GET /api/products
 // @access Public
 const getProducts = asyncHandler(async (req, res) => {
+  const pageSize = 10;
+  const page = Number(req.query.pageNumber) || 1;
+
   const keyword = req.query.keyword
     ? {
         name: {
@@ -14,8 +17,12 @@ const getProducts = asyncHandler(async (req, res) => {
       }
     : {};
 
-  const products = await Product.find({ ...keyword });
-  res.json(products);
+  const count = await Product.count({ ...keyword });
+
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
 // @desc Fetch one products
@@ -129,6 +136,15 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Get top rated products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Prouct.find({}).sort({ rating: -1 }).limit(5);
+
+  res.json(products);
+});
+
 export {
   getProducts,
   getProductById,
@@ -136,4 +152,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 };
