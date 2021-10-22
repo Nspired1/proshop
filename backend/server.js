c9843787843a7b0108c9ecd5656d91555fc25533
Don Spire <don.spire1@gmail.com>
2021-10-21 22:28:06 -0700

    fix typo in server.js

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ const folder = path.resolve();
 app.use("/uploads", express.static(path.join(folder, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(folder, "/frontend/build")));
+  app.use(express.static(path.join(folder, "/client/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(folder, "client/build/index.html"))
