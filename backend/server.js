7c6e2295656821c17340a1ed5e2425583b2fc110
Don Spire <don.spire1@gmail.com>
2021-10-22 01:20:19 -0700

    another fix

--- backend/server.js
+++ backend/server.js
@@ -42,9 +42,9 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(folder, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(folder, "/client/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(folder, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
 
