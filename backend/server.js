1c352a23f589edcd5dc73e4c62de1daa8275995d
Don Spire <don.spire1@gmail.com>
2021-10-22 10:01:28 -0700

    another fix

--- backend/server.js
+++ backend/server.js
@@ -47,6 +47,10 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
 }
 
 // handle routes Not Found
