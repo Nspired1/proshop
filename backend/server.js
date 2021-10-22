70c84d49871553f8d6d51fa3961768540a167495
Don Spire <don.spire1@gmail.com>
2021-10-21 22:58:01 -0700

    another fix for server.js

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,6 @@ app.use("/uploads", express.static(path.join(folder, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(folder, "client", "build", "index.html"))
   );
